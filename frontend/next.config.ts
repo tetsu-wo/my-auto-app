ce5a2a48b5d256755829848eef8d2ad049f375d3
mun <m.secheol.jp@gmail.com>
2026-06-02 14:17:10 +0900

    chore: next.config.tsの修正

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: path.join(__dirname, '.'),
+    root: __dirname,
   },
 };
 
 export default nextConfig;
+
 
