d3a6daf7ec369d7930d42e1383eeab13b262d790
mun <m.secheol.jp@gmail.com>
2026-06-03 14:33:04 +0900

    UserProfileの修正

--- frontend/app/components/UserProfile.tsx
+++ frontend/app/components/UserProfile.tsx
@@ -14,7 +14,7 @@ const UserProfile = () => {
 
   useEffect(() => {
     // JavaのバックエンドAPIからデータを取得
-    fetch("${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users/1")
+    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users/1`)
       .then((res) => res.json())
       .then((data: User) => {
         setUser(data);
